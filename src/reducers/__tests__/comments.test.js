25d415360594f2769ec3a418663ce2e8170dcda7
j2FunOnly <j2funonly@ya.ru>
2019-01-10 12:52:47 +0300

    Add test for comments reducer

---
+++ src/reducers/__tests__/comments.test.js
@@ -0,0 +1,13 @@
+import { SAVE_COMMENT } from 'actions/types';
+import commentsReducer from 'reducers/comments';
+
+it('handles actions of type SAVE_COMMENT', () => {
+  const action = {
+    type: SAVE_COMMENT,
+    payload: 'new comment'
+  };
+
+  const newState = commentsReducer([], action);
+
+  expect(newState).toEqual(['new comment']);
+});
