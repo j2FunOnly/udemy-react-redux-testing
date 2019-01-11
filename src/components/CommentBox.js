2ce5bf8526babdf81d11744f8f5181c3b67bfcc7
j2FunOnly <j2funonly@ya.ru>
2019-01-11 11:20:02 +0300

    Extract auth logic to requireAuth HOC

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,23 +2,10 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
   state = { comment: '' };
-
-  componentDidMount () {
-    this.shouldNavigateAway();
-  }
-
-  componentDidUpdate () {
-    this.shouldNavigateAway();
-  }
-
-  shouldNavigateAway () {
-    if (this.props.auth) return;
-
-    this.props.history.push('/');
-  }
 
   handleChange = event => {
     this.setState({comment: event.target.value})
@@ -48,8 +35,4 @@ class CommentBox extends Component {
   }
 }
 
-function mapStateToProps (state) {
-  return { auth: state.auth }
-}
-
-export default connect(mapStateToProps, actions)(CommentBox);
+export default connect(null, actions)(requireAuth(CommentBox));
